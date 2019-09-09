1f3f1ea4b788c9478663efd730f3067dd0e7affc
Максим Шепиль <mshepil@opvf.ru>
2019-09-09 17:52:03 +0300

    adding tests for class Game

---
+++ src/Game.js
@@ -0,0 +1,19 @@
+
+export default class Game {
+
+    constructor(){
+        this._board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+          ];
+    }
+
+    getState(){
+        return this._board;
+    }
+
+    acceptUserMove(x, y){
+        this._board[0][0] = 'x';
+    }
+}
